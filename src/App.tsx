1281df997ecd5affa0dcbc09afb7ff67502c75e8
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-01-01 19:58:04 +0530

    Refactor App component to use lazy loading for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home';
-import CarDetails from './pages/CarDetails';
-import Cars from './pages/Cars';
-import MyBookings from './pages/MyBookings';
-import Footer from './components/Footer';
-import Layout from './pages/owner/Layout';
-import Dashboard from './pages/owner/Dashboard';
-import AddCar from './pages/owner/AddCar';
-import ManageCars from './pages/owner/ManageCars';
-import ManageBookings from './pages/owner/ManageBookings';
-import Login from './pages/Login';
 import { Toaster } from 'react-hot-toast';
 import { useAppContext } from './context/AppContext';
+import Footer from './components/Footer';
+import Layout from './pages/owner/Layout';
+
+import  { lazy, Suspense, type ReactNode } from 'react'
+
+// import Home from './pages/Home';
+// import CarDetails from './pages/CarDetails';
+// import Cars from './pages/Cars';
+// import MyBookings from './pages/MyBookings';
+// import Dashboard from './pages/owner/Dashboard';
+// import AddCar from './pages/owner/AddCar';
+// import ManageCars from './pages/owner/ManageCars';
+// import ManageBookings from './pages/owner/ManageBookings';
+// import Login from './pages/Login';
+const Home = lazy(() => import("./pages/Home"));
+const CarDetails = lazy(() => import("./pages/CarDetails"));
+const Cars = lazy(() => import("./pages/Cars"));
+const MyBookings = lazy(() => import("./pages/MyBookings"));
+const Dashboard = lazy(() => import("./pages/owner/Dashboard"));
+const AddCar = lazy(() => import("./pages/owner/AddCar"));
+const ManageCars = lazy(() => import("./pages/owner/ManageCars"));
+const ManageBookings = lazy(() => import("./pages/owner/ManageBookings"));
+const Login = lazy(() => import("./pages/Login"));
 
 const App = () => {
 
@@ -25,18 +37,21 @@ const App = () => {
       {showLogin && <Login />}
       {!isOwnerPath && <Navbar />}
 
-      <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/car-details/:id' element={<CarDetails />} />
-          <Route path='/cars' element={<Cars />} />
-          <Route path='/my-bookings' element={<MyBookings />} />
-          <Route path='/owner' element={<Layout />}>
-              <Route index element={<Dashboard />}/>
-              <Route path='add-car' element={<AddCar />}/>
-              <Route path='manage-cars' element={<ManageCars />}/>
-              <Route path='manage-bookings' element={<ManageBookings />}/>
-          </Route>
-      </Routes>
+      {/* IMPORTANT: wrap lazy components */}
+      <Suspense fallback={<h2 style={{textAlign:'center'}}>Loading...</h2>}>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/car-details/:id' element={<CarDetails />} />
+            <Route path='/cars' element={<Cars />} />
+            <Route path='/my-bookings' element={<MyBookings />} />
+            <Route path='/owner' element={<Layout />}>
+                <Route index element={<Dashboard />}/>
+                <Route path='add-car' element={<AddCar />}/>
+                <Route path='manage-cars' element={<ManageCars />}/>
+                <Route path='manage-bookings' element={<ManageBookings />}/>
+            </Route>
+        </Routes>
+      </Suspense>
 
 
       {!isOwnerPath && <Footer /> }
