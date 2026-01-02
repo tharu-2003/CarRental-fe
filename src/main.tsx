d229b88342096db4d75307a4b6a3ea773a27b370
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-01-02 21:51:03 +0530

    Refactor main.tsx to enhance motion configuration and improve component structure

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,21 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './context/AppContext.tsx'
-import {MotionConfig} from 'motion/react'
+import { MotionConfig, motion } from 'framer-motion';
+// import {MotionConfig} from 'motion/react'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <AppProvider>
-      <MotionConfig viewport={{once: true}}>
-        <App />
+      <MotionConfig>
+        <motion.div viewport={{ once: true }}>
+          <App />
+        </motion.div>
       </MotionConfig>
     </AppProvider>
   </BrowserRouter>,
 )
+
+
+
+
