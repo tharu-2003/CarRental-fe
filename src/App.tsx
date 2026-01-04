fb0f158b67939951ba419d8a4ce5510cef6a9a28
tharu-2003 <sandaruwantharusha968@gmail.com>
2026-01-04 10:39:40 +0530

    check app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast';
-import { useAppContext } from './context/AppContext';
+// import { useAppContext } from './context/AppContext';
 import Footer from './components/Footer';
 import Layout from './pages/owner/Layout';
 
@@ -16,19 +16,19 @@ const Dashboard = lazy(() => import("./pages/owner/Dashboard"));
 const AddCar = lazy(() => import("./pages/owner/AddCar"));
 const ManageCars = lazy(() => import("./pages/owner/ManageCars"));
 const ManageBookings = lazy(() => import("./pages/owner/ManageBookings"));
-const Login = lazy(() => import("./pages/Login"));
+// const Login = lazy(() => import("./pages/Login"));
 const ResetPassword = lazy(() => import("./pages/ResetPassword"))
 
 const App = () => {
 
-  const {showLogin} = useAppContext()
+  // const {showLogin} = useAppContext()
   const isOwnerPath = useLocation().pathname.startsWith('/owner');
 
   return (
     <>
      <Toaster />
-      {showLogin && <Login />}
-      {!isOwnerPath && <Navbar />}
+      {/* {showLogin && <Login />}
+      {!isOwnerPath && <Navbar />} */}
 
       {/* IMPORTANT: wrap lazy components */}
       {/* <Suspense fallback={<h2 style={{textAlign:'center'}}>Loading...</h2>}> */}
